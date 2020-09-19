712cd177e8e98f41a0f1689a92523e153bd3902d
Petr Zed <petr.zinovyev@devexpress.com>
2020-09-19 23:38:56 +0300

    Start

---
+++ CommonLib/CommonLib/CommonClasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib {
+    public class VsixSend {
+        public VsixSend(ActionType actionType) {
+            ActionType = actionType;
+        }
+
+        public ActionType ActionType { get; set; }
+    }
+    public enum ActionType {
+        AddFile,
+        AddReference,
+    }
+
+    public class VsixResponse {
+        public VsixResponse(bool success) {
+            Success = success;
+        }
+
+        public bool Success { get; set; }
+    }
+}
